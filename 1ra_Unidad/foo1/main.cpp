9c4fa975aeb42280c437528eed6c47844ea6a83c
ltch23 <luistong23@gmail.com>
2017-03-16 20:35:04 -0500

    foo1

---
+++ 1ra_Unidad/foo1/main.cpp
@@ -0,0 +1,15 @@
+#This is a foo1
+#Luis Tong Chabes
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Hola Algoritmos Paralelos"<<endl;
+    cout<<"Hello Parallel Computing"<<endl;
+    
+    return 0
+    
+};
+    
